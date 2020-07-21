0c5d409271b519381e92a75507d4cb5112818df8
LeeWalpole <lee@ladsholidayguide.com>
2020-07-21 15:37:18 +0700

    index fix

--- index.php
+++ index.php
@@ -1,7 +1,91 @@
 <?php get_header(); // includes nav and hero ?>
 
+<?php if ( have_posts() ) : ?>
+<div class="bg-color">
+    <div class="grid grid-gap w-max">
+
+        <?php while ( have_posts() ) : the_post();  ?>
+
+        <?php
+$category = get_the_category();
+$kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
+$headline = get_field('hero_headline') ?: get_the_title(); 
+$subdeck = get_field('hero_subdeck'); // for some reason this didn't work
+$teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
+$feature_youtube = get_field('feature_youtube'); 
+?>
+
+        <?php $i++; if($i <= 1 ) : ?>
+
+        <article class="teaser teaser_standard bg-white colspan-7">
+
+            <a href="https://www.lads.guide/learn/how-to-clean-shave/" title="Learn how to clean shave"
+                value="Learn how to clean shave">
+                <figure class="bg-white prefade ratio <?php if ($feature_youtube) : echo "video_teaser"; endif; ?>"
+                    data-ratio="3x2">
+                    <picture>
+                        <source type="image/jpeg" media="(min-width: 461px)"
+                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'medium')); ?>">
+                        <source type="image/jpeg" media="(max-width: 460px)"
+                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'thumbnail')); ?>">
+                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
+                    </picture>
+                </figure>
+                <header class="header bg-white postfade">
+                    <strong class="kicker">Gillette</strong>
+                    <h6 class="headline">Learn how to clean shave</h6>
+                    <p>Want to learn how to clean shave?</p>
+                </header>
+            </a>
+            <?php endif; ?>
+        </article>
+
+
+        <div class="puff_teasers colspan-5 bg-white">
+
+
+            <?php if($i >=2 && $i <=5) :?>
+
+            <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
+                class="puff_teaser bg-white">
+                <figure class="<?php if ($feature_youtube) : echo "video_teaser"; endif; ?>">
+                    <picture>
+                        <source type="image/jpeg" media="(min-width: 461px)"
+                            srcset="<?php echo esc_attr($teaser_image_url); ?>">
+                        <source type="image/jpeg" media="(max-width: 460px)"
+                            srcset="<?php echo esc_attr($teaser_image_url); ?>">
+                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
+                    </picture>
+                </figure>
+                <header class="header">
+                    <strong class="kicker"><?php echo $kicker; ?></strong>
+                    <h6 class="headline"><?php echo $headline; ?></h6>
+                </header>
+            </a>
+
+            <?php endif; ?>
+        <?php endwhile; ?>
+        </div>
+    </div>
+</div>
+
+<?php endif; ?>
+
+
+
+
+
+
+
 <div>
     <?php get_template_part( 'snippets/snippet-hero' ); ?>
+
+
+
+
+
 
     <?php if ( have_posts() ) : ?>
     <div class="w-max">
