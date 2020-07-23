282bde6801492ad97f9bdd33731f847f8a566809
LeeWalpole <lee@ladsholidayguide.com>
2020-07-23 10:43:58 +0700

    font weight

--- snippets/snippet-fonts.php
+++ snippets/snippet-fonts.php
@@ -33,24 +33,24 @@
 	font-family: 'Geomanist';
     src: url('".get_template_directory_uri()."/fonts/Geomanist-Regular.woff2') format('woff2'),
 	url('".get_template_directory_uri()."/fonts/Geomanist-Regular.woff') format('woff');
-	font-weight: 400;
+	font-weight: normal;
 	font-style: normal;
 	font-display:swap;
 }
 @font-face {
-	font-family: 'Geomanist-Bold';
+	font-family: 'GeomanistBold';
 	src: url('".get_template_directory_uri()."/fonts/Geomanist-Bold.woff2') format('woff2'),
 	url('".get_template_directory_uri()."/fonts/Geomanist-Bold.woff') format('woff');
-	font-weight: bold;
+	font-weight: normal;
 	font-style: normal;
 	font-display:swap;
 }
 
 @font-face {
-	font-family: 'Geomanist-Heavy';
+	font-family: 'GeomanistHeavy';
 	src: url('".get_template_directory_uri()."/fonts/Geomanist-Black.woff2') format('woff2'),
 	url('".get_template_directory_uri()."/fonts/Geomanist-Black.woff') format('woff');
-	font-weight: 900;
+	font-weight: normal;
 	font-style: normal;
 	font-display:swap;
 }
@@ -61,8 +61,8 @@
 --font-1: 'Geomanist', Helvetica, Arial, sans-serif;    
 --font-2: 'Geomanist', Helvetica, Arial, sans-serif;    
 --font-normal: 'Geomanist', Helvetica, Arial, sans-serif;    
---font-bold: 'Geomanist-Bold', Helvetica, Arial, sans-serif;    
---font-heavy: 'Geomanist-Heavy', Helvetica, Arial, sans-serif;    
+--font-bold: 'GeomanistBold', Helvetica, Arial, sans-serif;    
+--font-heavy: 'GeomanistHeavy', Helvetica, Arial, sans-serif;    
 --regular: 400;    
 --bold: bold;    
 --heavy: 900;
