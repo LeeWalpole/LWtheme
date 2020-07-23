1a02e97f5e4595fde154d06ab5ef5d763786313b
LeeWalpole <lee@ladsholidayguide.com>
2020-07-23 11:00:25 +0700

    snippet related

--- snippets/snippet-related.php
+++ snippets/snippet-related.php
@@ -1,22 +1,17 @@
 <?php     
-$teaser_count = -1;
-$teaser_category = get_sub_field('category') ?: "More like this";
-$teaser_tag = get_sub_field('tag');
+$teaser_category = "More like this";
 $category = get_the_category();
-$category_link = get_category_link( $teaser_category) ?: get_category_link( $category[0]->term_id );
-$offset = 0;
-$layout = get_sub_field('layout');
-$showcase_limit = get_sub_field('showcase_limit') ?: "-1";
-$showcase_heading = get_sub_field('showcase_heading') ?: "MORE LIKE THIS";
+$category_link = get_category_link( $category[0]->term_id );
+$showcase_heading = "MORE LIKE THIS";
 ?>
 
 <?php 
 $posts = get_posts(array(
-'post_type'			=> 'post',
 'posts_per_page' => 1,
 'offset' => 0,
 'category__in' => wp_get_post_categories( $post->ID ), 
 'post__not_in' => array( $post->ID ) ,
+// 'post_type'			=> 'post',
 ) ); 
 if( $posts ): ?>
 
@@ -33,7 +28,7 @@ if( $posts ): ?>
 
         <article class="teaser standard_teaser teaser_highlight colspan-7">
 
-            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+            <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
             <?php
 $category = get_the_category();
 $kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
@@ -43,7 +38,6 @@ $teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
 $feature_youtube = get_field('feature_youtube'); 
 ?>
 
-            <?php $i++; if($i <= 1 ) : ?>
             <a href="https://www.lads.guide/learn/how-to-clean-shave/" title="Learn how to clean shave"
                 value="Learn how to clean shave">
                 <figure class="prefade ratio <?php if ($feature_youtube) : echo "video_teaser"; endif; ?>"
@@ -63,7 +57,6 @@ $feature_youtube = get_field('feature_youtube');
                     <?php if($subdeck) : ?><p class="subdeck"><?php echo $subdeck; ?></p><?php endif; ?>
                 </header>
             </a>
-            <?php endif; // first post ?>
             <?php endforeach; ?>
         </article>
         <?php endif; // have_posts ?>
@@ -80,7 +73,7 @@ $posts = get_posts(array(
 if( $posts ): ?>
 
         <div class="puff_teasers colspan-5 bg-offwhite">
-            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+            <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
             <?php
 $category = get_the_category();
 $kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
@@ -110,64 +103,6 @@ $feature_youtube = get_field('feature_youtube');
     </div>
 </div>
 <!-- end have posts below -->
-<?php endif; // have_posts ?>
-
-<?php wp_reset_postdata(); ?>
-
-<?php if ($layout == "showcase_all" || $layout == "showcase_limit")  { ?>
-<!-- Showcase ALL -->
-
-<?php get_template_part( 'snippets/snippet-search' ); ?>
-
-<?php 
-$posts = get_posts(array(
-'post_type'			=> 'post',
-'posts_per_page' => $showcase_limit,
-'tag_id' => $teaser_tag, 
-'category' => $teaser_category ) ); 
-if( $posts ): ?>
-
-<div class="bg-offwhite row-block">
-    <div class="w-max grid grid-gap padding-x teasers standard_teasers">
-
-        <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
-
-        <?php
-$category = get_the_category();
-$kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
-$headline = get_field('hero_headline') ?: get_the_title(); 
-$subdeck = get_field('hero_subdeck'); // for some reason this didn't work
-$teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
-$feature_youtube = get_field('feature_youtube'); 
-?>
-
-        <article class="teaser standard_teaser">
-            <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
-                value="<?php echo esc_attr($headline); ?>">
-                <figure class="prefade ratio <?php if ($feature_youtube) : echo "video_teaser"; endif; ?>"
-                    data-ratio="3x2">
-                    <picture>
-                        <source type="image/jpeg" media="(min-width: 461px)"
-                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'medium')); ?>">
-                        <source type="image/jpeg" media="(max-width: 460px)"
-                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'thumbnail')); ?>">
-                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
-                    </picture>
-                </figure>
-                <header class="header prefade">
-                    <strong class="kicker"><?php echo $kicker; ?></strong>
-                    <h6 class="headline"><?php echo $headline; ?></h6>
-                    <?php if ($subdeck) : ?><p><?php echo $subdeck; ?></p><?php endif; ?>
-                </header>
-            </a>
-        </article>
-        <?php endforeach; ?>
-    </div>
-</div>
-<?php endif; // have_posts ?>
-<?php } else  { ?>
-<!-- Showcase 5 Only -->
-<?php } // $showcase == "showcase_all" || "showcase_limit")  ?>
+<?php endif; // posts querie ?>
 
 <?php wp_reset_postdata(); ?>
