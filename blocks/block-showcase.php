d3c11064b3589f6dc9df104bf867caca440e9f20
LeeWalpole <lee@ladsholidayguide.com>
2020-07-26 21:54:28 +0700

    oild showcase

--- blocks/block-showcase.php
+++ blocks/block-showcase.php
@@ -1,112 +1,3 @@
-<?php 
-$posts = get_posts(array(
-'post_type'			=> 'post',
-'posts_per_page' => 1,
-'offset' => 0
-)); 
-if( $posts ): ?>
-
-<div class="bg-white row-block">
-    <div class="grid grid-gap w-max showcase">
-
-        <a href="#" title="" class="colspan-12">
-            <header class="showcase_header header">
-                <i class="fas fa-arrow-circle-right color"></i>
-                <h2 class="headline">
-NEW POSTS
-                </h2>
-            </header>
-        </a>
-
-        <article class="teaser standard_teaser teaser_highlight bg-white colspan-7">
-            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post );
-            ?>
-            <?php
-$category = get_the_category();
-$kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
-$headline = get_field('hero_headline') ?: get_the_title(); 
-$subdeck = get_field('hero_subdeck'); // for some reason this didn't work
-$teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
-$feature_youtube = get_field('feature_youtube'); 
-$splash_id = $post->ID;
-?>
-            <?php $i++; if($i <= 1 ) : ?>
-            <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
-                value="<?php echo esc_attr($headline); ?>">
-                <figure class="bg-white prefade ratio <?php if ($feature_youtube) : echo "video_teaser"; endif; ?>"
-                    data-ratio="3x2">
-                    <picture>
-                        <source type="image/jpeg" media="(min-width: 461px)"
-                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'medium')); ?>">
-                        <source type="image/jpeg" media="(max-width: 460px)"
-                            srcset="<?php echo esc_url(get_the_post_thumbnail_url($post->ID, 'thumbnail')); ?>">
-                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
-                    </picture>
-                </figure>
-                <header class="header bg-white postfade">
-                    <strong class="kicker"><?php echo $kicker; echo $splash_id; ?></strong>
-                    <h6 class="headline"><?php echo $headline; ?></h6>
-                    <?php if($subdeck) : ?><p class="subdeck"><?php echo $subdeck; ?></p><?php endif; ?>
-                </header>
-            </a>
-            <?php endif; // first post ?>
-            <?php endforeach; ?>
-        </article>
-        <?php endif; // have_posts ?>
-
-        <?php wp_reset_postdata(); ?>
-
-        <?php 
-$posts = get_posts(array(
-'post_type' => 'post',
-'posts_per_page' => 4,
-'post__not_in' => $splash_id,
-'exclude' => $splash_id,
-'offset' => 1,
-)); 
-if( $posts ): ?>
-
-        <div class="puff_teasers colspan-5 bg-white">
-            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post ); ?>
-            <?php
-$category = get_the_category();
-$kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
-$headline = get_field('hero_headline') ?: get_the_title(); 
-$subdeck = get_field('hero_subdeck'); // for some reason this didn't work
-$teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
-$feature_youtube = get_field('feature_youtube'); 
-?>
-
-            <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
-                class="puff_teaser teaser bg-white">
-                <figure>
-                    <picture>
-                        <source type="image/jpg" srcset="<?php echo esc_attr($teaser_image_url); ?>">
-                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
-                    </picture>
-                </figure>
-                <header class="header">
-                    <strong class="kicker"><?php echo $kicker; ?></strong>
-                    <h6 class="headline"><?php echo $headline; ?></h6>
-                </header>
-            </a>
-            <?php endforeach; ?>
-            <!-- endwhile above -->
-        </div><!-- puff_teasers -->
-    </div>
-</div>
-<!-- end have posts below -->
-<?php endif; // have_posts ?>
-
-<?php wp_reset_postdata(); ?>
-
-
-<hr>
-
-
-
 <?php     
 $teaser_count = -1;
 $teaser_category = get_sub_field('category') ?: "no category";
@@ -132,7 +23,8 @@ if( $posts ): ?>
     <div class="grid grid-gap w-max showcase">
 
         <?php if ($showcase_heading) : ?>
-        <a href="<?php echo esc_url($category_link); ?>" title="<?php echo esc_attr($showcase_heading); ?>" class="colspan-12">
+        <a href="<?php echo esc_url($category_link); ?>" title="<?php echo esc_attr($showcase_heading); ?>"
+            class="colspan-12">
             <header class="showcase_header header">
                 <i class="fas fa-arrow-circle-right color"></i>
                 <h2 class="headline">
