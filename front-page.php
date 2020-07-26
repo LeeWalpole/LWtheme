71a4781cbbb2d450d59eb43fe84aea6ca69044d9
LeeWalpole <lee@ladsholidayguide.com>
2020-07-26 21:48:05 +0700

    no duup posts

--- front-page.php
+++ front-page.php
@@ -2,17 +2,6 @@
 
 <?php get_template_part( 'snippets/snippet-hero' ); ?>
 
-<?php     
-$teaser_count = -1;
-$teaser_category = get_sub_field('category') ?: "no category";
-$teaser_tag = get_sub_field('tag');
-$category_link = get_category_link( $teaser_category) ?: "#";
-$offset = 0;
-$layout = get_sub_field('layout');
-$showcase_limit = get_sub_field('showcase_limit') ?: "-1";
-$showcase_heading = get_sub_field('showcase_heading');
-?>
-
 <?php 
 $posts = get_posts(array(
 'post_type'			=> 'post',
@@ -21,7 +10,7 @@ $posts = get_posts(array(
 )); 
 if( $posts ): ?>
 
-<div class="bg-black row-block">
+<div class="bg-white row-block">
     <div class="grid grid-gap w-max showcase">
 
         <a href="#" title="" class="colspan-12">
@@ -43,8 +32,7 @@ $headline = get_field('hero_headline') ?: get_the_title();
 $subdeck = get_field('hero_subdeck'); // for some reason this didn't work
 $teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
 $feature_youtube = get_field('feature_youtube'); 
-$splash_ids = $post->ID;
-$postids = get_the_ID();
+$splash_id = $post->ID;
 ?>
             <?php $i++; if($i <= 1 ) : ?>
             <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
@@ -63,8 +51,6 @@ $postids = get_the_ID();
                 <header class="header bg-white postfade">
                     <strong class="kicker"><?php echo $kicker; echo $splash_id; ?></strong>
                     <h6 class="headline"><?php echo $headline; ?></h6>
-                    <p>Splash ID: <?php echo $splash_ids; ?> </p>
-                    <p>Post ID: <?php echo $post_ids; ?> </p>
                     <?php if($subdeck) : ?><p class="subdeck"><?php echo $subdeck; ?></p><?php endif; ?>
                 </header>
             </a>
@@ -81,8 +67,7 @@ $posts = get_posts(array(
 'posts_per_page' => 4,
 'post__not_in' => $splash_id,
 'offset' => 1,
-'tag_id' => $teaser_tag, 
-'category' => $teaser_category ) ); 
+)); 
 if( $posts ): ?>
 
         <div class="puff_teasers colspan-5 bg-white">
@@ -121,35 +106,44 @@ $feature_youtube = get_field('feature_youtube');
 <?php wp_reset_postdata(); ?>
 
 
-<?php if ($layout == "showcase_all" || $layout == "showcase_limit")  { ?>
-<!-- Showcase ALL -->
 
-<?php get_template_part( 'snippets/snippet-search' ); ?>
+
+
 
 <?php 
 $posts = get_posts(array(
 'post_type'			=> 'post',
-'posts_per_page' => $showcase_limit,
+'posts_per_page' => 1,
 'post__not_in' => $splash_id,
-'tag_id' => $teaser_tag, 
-'category' => $teaser_category ) ); 
+'offset' => 0
+)); 
 if( $posts ): ?>
 
-<div class="bg-offwhite row-block">
-    <div class="w-max grid grid-gap padding-x teasers standard_teasers">
+<div class="bg-white row-block">
+    <div class="grid grid-gap w-max showcase">
 
-        <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+        <a href="#" title="" class="colspan-12">
+            <header class="showcase_header header">
+                <i class="fas fa-arrow-circle-right color"></i>
+                <h2 class="headline">
+two POSTS
+                </h2>
+            </header>
+        </a>
 
-        <?php
+        <article class="teaser standard_teaser teaser_highlight bg-white colspan-7">
+            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post );
+            ?>
+            <?php
 $category = get_the_category();
 $kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
 $headline = get_field('hero_headline') ?: get_the_title(); 
 $subdeck = get_field('hero_subdeck'); // for some reason this didn't work
 $teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
 $feature_youtube = get_field('feature_youtube'); 
+$splash_id = $post->ID;
 ?>
-
-        <article class="teaser standard_teaser bg-white">
+            <?php $i++; if($i <= 1 ) : ?>
             <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
                 value="<?php echo esc_attr($headline); ?>">
                 <figure class="bg-white prefade ratio <?php if ($feature_youtube) : echo "video_teaser"; endif; ?>"
@@ -163,22 +157,75 @@ $feature_youtube = get_field('feature_youtube');
                             alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
                     </picture>
                 </figure>
-                <header class="header bg-white prefade">
-                    <strong class="kicker"><?php echo $kicker; ?></strong>
+                <header class="header bg-white postfade">
+                    <strong class="kicker"><?php echo $kicker; echo $splash_id; ?></strong>
                     <h6 class="headline"><?php echo $headline; ?></h6>
-                    <?php if ($subdeck) : ?><p><?php echo $subdeck; ?></p><?php endif; ?>
+                    <?php if($subdeck) : ?><p class="subdeck"><?php echo $subdeck; ?></p><?php endif; ?>
                 </header>
             </a>
+            <?php endif; // first post ?>
+            <?php endforeach; ?>
         </article>
-        <?php endforeach; ?>
+        <?php endif; // have_posts ?>
+
+        <?php wp_reset_postdata(); ?>
+
+        <?php 
+$posts = get_posts(array(
+'post_type' => 'post',
+'posts_per_page' => 4,
+'post__not_in' => $splash_id,
+'offset' => 1,
+)); 
+if( $posts ): ?>
+
+        <div class="puff_teasers colspan-5 bg-white">
+            <?php $i=0; foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+            <?php
+$category = get_the_category();
+$kicker = get_field('hero_kicker') ?: $category[0]->cat_name;
+$headline = get_field('hero_headline') ?: get_the_title(); 
+$subdeck = get_field('hero_subdeck'); // for some reason this didn't work
+$teaser_image_url = get_the_post_thumbnail_url($post->ID, 'puff');
+$feature_youtube = get_field('feature_youtube'); 
+?>
+
+            <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
+                class="puff_teaser teaser bg-white">
+                <figure>
+                    <picture>
+                        <source type="image/jpg" srcset="<?php echo esc_attr($teaser_image_url); ?>">
+                        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                            alt="<?php echo esc_attr($headline); ?>" class="lazyload" loading="lazy">
+                    </picture>
+                </figure>
+                <header class="header">
+                    <strong class="kicker"><?php echo $kicker; ?></strong>
+                    <h6 class="headline"><?php echo $headline; ?></h6>
+                </header>
+            </a>
+            <?php endforeach; ?>
+            <!-- endwhile above -->
+        </div><!-- puff_teasers -->
     </div>
 </div>
+<!-- end have posts below -->
 <?php endif; // have_posts ?>
-<?php } else  { ?>
-<!-- Showcase 5 Only -->
-<?php } // $showcase == "showcase_all" || "showcase_limit")  ?>
 
 <?php wp_reset_postdata(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <?php get_template_part( 'blocks/blocks' ); ?>
