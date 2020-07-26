137b9dd0c755a13d425c1c76b4507d308e28429c
LeeWalpole <lee@ladsholidayguide.com>
2020-07-26 21:16:36 +0700

    tes.....

--- front-page.php
+++ front-page.php
@@ -4,25 +4,6 @@
 
 <?php get_template_part( 'snippets/snippet-hero' ); ?>
 
-<?php $my_query = new WP_Query('category_name=featured&posts_per_page=5');
-  while ($my_query->have_posts()) : $my_query->the_post();
- 
-  $do_not_duplicate = $post->ID; //This is the magic line
- 
-?>
-<p>Title: 1: <?php the_title(); ?></p>
-    <!-- Do stuff... -->
-<?php endwhile; ?>
-
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); 
- 
-  if( $post->ID == $do_not_duplicate ) continue; //This is the Magic Line
- 
- ?>
-<p>Title: 2: <?php the_title(); ?></p>
-  <?php endwhile; endif; ?>
-
 <?php get_template_part( 'blocks/blocks' ); ?>
 
 <?php get_template_part( 'snippets/newsletter' ); ?>
