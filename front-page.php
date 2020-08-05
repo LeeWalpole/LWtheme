e389ba17f31ab3c52f9dd28a7f13daea2a6b4d5c
LeeWalpole <lee@ladsholidayguide.com>
2020-08-05 14:32:08 +0700

    homepage duplicate posts

--- front-page.php
+++ front-page.php
@@ -2,6 +2,32 @@
 
 <?php get_template_part( 'snippets/snippet-hero' ); ?>
 
+<?php
+$do_not_duplicate = array(); // set befor loop variable as array
+
+// 1. Loop
+query_posts('showposts=5');
+while ( have_posts() ) : the_post();
+    $do_not_duplicate[] = $post->ID; // remember ID's in loop
+    // display post ...
+    "<p>".the_title()."</p>";
+endwhile;
+?>
+<hr>
+<?php
+// 2. Loop
+query_posts( 'showposts=15' );
+while (have_posts()) : the_post();
+    if ( !in_array( $post->ID, $do_not_duplicate ) ) { // check IDs         
+// display posts ...
+"<p>".the_title()."</p>";
+    }
+endwhile;
+?>
+
+
+
+
 <?php get_template_part( 'blocks/blocks' ); ?>
 
 <?php get_template_part( 'snippets/newsletter' ); ?>
