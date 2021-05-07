9dc5fd114863b23caa1eefd3cbb933c47fb798ac
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-07 13:38:44 +0700

    Temp Hack: added CSS to Hero signup form

--- snippets/snippet-hero.php
+++ snippets/snippet-hero.php
@@ -54,6 +54,11 @@ $hero_subdeck = get_field('hero_subdeck'); // for some reason this didn't work
             <?php if ($hero_subdeck) : ?><em class="subdeck"><?php echo $hero_subdeck; ?></em><?php endif; ?>
 
 <?php if(is_front_page()) : ?>
+
+    <style>
+:root{--color:#ee0099!important;--color-highlight:#ff0099!important}.grid-hero-signup button,.grid-hero-signup input{grid-gap:var(--padding-small);height:50px;line-height:50px;width:100%;font-size:var(--small)}.grid-hero-signup input{padding:var(--padding-small)}.grid-hero-signup{padding:0;grid-gap:var(--padding)}.hero-signup{padding:var(--padding-small) 0}.hero-signup .submit{margin-top:var(--padding-small)}.hero-signup .button{width:100%;cursor:pointer;background-color:var(--color);color:#fff;border:none;-webkit-appearance:none}.hero-signup .button:hover{background-color:var(--color)}.hero-signup .button:hover{background:var(--color-highlight)}.agile-error-msg span{color:var(--color)!important;font-size:var(--small)!important;margin-top:var(--padding-xsmall)}.checkboxes{width:100%;height:28px;display:flex;align-items:center;justify-content:space-between;color:var(--color-white);line-height:25px}.checkbox-wrap{position:relative;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;width:100%;line-height:25px}.checkbox-wrap p{position:absolute;left:calc(22px + var(--padding-small));line-height:25px;font-size:var(--small);display:inline-block;color:var(--color-white)}.checkbox-wrap input{position:absolute;left:0;top:0;opacity:0;cursor:pointer;height:0;width:0}.checkmark{position:absolute;top:0;left:0;height:25px;width:25px;background-color:var(--color-light)}.checkbox-wrap:hover input~.checkmark{background-color:var(--color-offwhite)}.checkbox-wrap input:checked~.checkmark{background-color:var(--color)}.checkmark:after{content:"";position:absolute;display:none}.checkbox-wrap input:checked~.checkmark:after{display:block}.checkbox-wrap .checkmark:after{left:9px;top:5px;width:5px;height:10px;border:solid #fff;border-width:0 3px 3px 0;-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);transform:rotate(45deg)}
+                </style>
+
                     <!-- HERO SIGNUP -->
                     <div class="hero-signup">
                         <form class="" id="agile-form" action="https://leewalpole.agilecrm.com/formsubmit" method="GET">
