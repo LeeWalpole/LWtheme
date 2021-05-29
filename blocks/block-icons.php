bab6b76045ffb838d16a9be409edd244e3dc97aa
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-30 00:28:55 +0700

    moved while

--- blocks/block-icons.php
+++ blocks/block-icons.php
@@ -91,14 +91,14 @@ text-align: center;
 
 
 <?php if( have_rows('icons') ): ?>
-<?php while( have_rows('icons') ): the_row(); ?>
 <div id="<?php echo esc_attr($block_id); ?>" class="row-block prefade <?php echo esc_attr($bg_color); ?>">
     <section class="<?php echo esc_attr($width); ?> grid">
         <!-- .article-block -->
         <article class="article colspan-12">
+            
             <?php include ('block-header.php'); ?>
             <div class="icons <?php echo $bg_color; ?>">
-
+            <?php while( have_rows('icons') ): the_row(); ?>
                 <?php if( have_rows('icon') ): ?>
                 <?php while( have_rows('icon') ): the_row(); ?>
              
@@ -127,7 +127,7 @@ $link_title = $link['title'];
                     </figcaption>
                     <?php endif; //headine ?>
                 </figure>
-
+                <?php endwhile; ?>
 
                 <?php endwhile; ?>
                 <?php endif; ?>
@@ -135,7 +135,7 @@ $link_title = $link['title'];
         </article>
     </section>
 </div>
-<?php endwhile; ?>
+
 <?php endif; ?>
 
 
