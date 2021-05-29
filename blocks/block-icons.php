92dbed03c0599014670f6b5bb8ee1a5a9b430727
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-29 23:28:59 +0700

    heelo and goodbye again

--- blocks/block-icons.php
+++ blocks/block-icons.php
@@ -1,13 +1,31 @@
+<h1>block-icons.php (below)</h1>
+
 <?php include ('block-settings.php'); ?>
 
-<?php if( have_rows('teaser_settings') ) : while( have_rows('teaser_settings') ) : the_row();?>
-<?php
-$teaser_styles = get_sub_field('teaser_styles'); // this works
-$teaser_ratio = get_sub_field('teaser_ratio'); // this works
-?>
-<?php endwhile; endif; ?>
+<div id="<?php // echo esc_attr($block_id); ?>" class="row-block prefade <?php // echo esc_attr($bg_color); ?>">
+    <section class="<?php // echo esc_attr($width); ?> grid"><!-- .article-block -->
+        <article class="article colspan-12">
+            <?php // include ('block-header.php'); ?>
+            <div class="grid icons <?php // echo $bg_color; ?>">
+                <?php //  include ('block-icon.php'); ?>
 
-<?php switch ($teaser_ratio) : case "3x2": ?>
+hello and goodbye
+
+                
+            </div>
+        </article>
+    </section>
+</div>
+
+<p>test</p>
+
+<?php
+/*
+include ('block-settings.php'); ?>
+
+<?php if( have_rows('icon_settings') ) :  while( have_rows('icon_settings') ): the_row(); ?>
+<?php $teaser_ratio = get_sub_field('teaser_ratio');?>
+    <?php switch ($teaser_ratio) : case "3x2": ?>
     <?php $ratio = "3:2"; 
     $teaser_image_height = 240;
     ?>
@@ -21,19 +39,37 @@ $teaser_ratio = get_sub_field('teaser_ratio'); // this works
     <?php $ratio = "3:2"; 
     $teaser_image_height = 240;
     ?>
-<?php endswitch; ?>
+    <?php endswitch; ?>
+<?php endwhile; endif; //teaser_settings ?>
 
+
+
+<?php if( $block_layout == 'sidebar') { ?>
 <div id="<?php echo esc_attr($block_id); ?>" class="row-block prefade <?php echo esc_attr($bg_color); ?>">
     <section class="<?php echo esc_attr($width); ?> grid"><!-- .article-block -->
-        <article class="article colspan-12">
-            <?php include ('block-header.php'); ?>
-            <div class="standard-teasers teasers <?php echo $bg_color; ?>">
+        <aside class="sidebar colspan-4">
+            <div class="sidebar-sticky">
+                <?php include ('block-header.php'); ?>
+            </div>
+        </aside>
+        <article class="article colspan-8">
+            <div class="grid icons <?php echo $bg_color; ?>">
                 <?php include ('block-icon.php'); ?>
             </div>
         </article>
     </section>
 </div>
+<?php } else { ?>
+<div id="<?php echo esc_attr($block_id); ?>" class="row-block prefade <?php echo esc_attr($bg_color); ?>">
+    <section class="<?php echo esc_attr($width); ?> grid"><!-- .article-block -->
+        <article class="article colspan-12">
+            <?php include ('block-header.php'); ?>
+            <div class="grid icons <?php echo $bg_color; ?>">
+                <?php include ('block-icon.php'); ?>
+            </div>
+        </article>
+    </section>
+</div>
+<?php } */ // block layout ?>
 
-
-
-<?php } // block layout ?>
+<h1>block-icons.php (above)</h1>
