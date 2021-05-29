1186c3956be72f2d37b8eff36e7bb399d3020f7f
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-29 23:35:47 +0700

    $headline

--- blocks/block-icons.php
+++ blocks/block-icons.php
@@ -12,10 +12,72 @@
                 <p>test</p>
 
                 
+
+                
             </div>
         </article>
     </section>
 </div>
+
+<?php if( have_rows('icons') ): ?>
+    <?php while( have_rows('icons') ): the_row(); ?>
+        <?php if( have_rows('icons') ): ?>
+            <?php while( have_rows('icons') ): the_row(); ?>
+            Hello
+            <?php 
+$kicker = get_sub_field('kicker');
+$headline = get_sub_field('headline');
+$lead = get_sub_field('lead');
+$url = get_sub_field('url');
+$image = get_sub_field('image');
+?>
+
+<?php if( $headline ) : echo $headline; ?><?php endif; ?>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+
+
+
+<?php  $post; if( have_rows('icons') ) : ?>
+<?php while( have_rows('icons',$post->ID) ) : the_row();  ?>
+<?php // $counter = 0;?>
+<?php if( have_rows('icon',$post->ID) ) : while( have_rows('icon',$post->ID) ) : the_row(); $counter++; ?>
+<?php 
+$kicker = get_sub_field('kicker');
+$headline = get_sub_field('headline');
+$lead = get_sub_field('lead');
+$url = get_sub_field('url');
+$image = get_sub_field('image');
+?>
+<?php 
+$link = get_sub_field('url');
+$link_target = $link['target'] ? $link['target'] : '_self'; 
+$link_title = $link['title'];
+?>
+
+<figure class="colspan-4 mobspan-6 icon">
+    <a href="<?php echo $link['url']; ?>" title="<?php echo esc_attr($link_title); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+        <!-- <i class="fas fa-beer grow icon-large"></i> -->
+        <?php echo $image; ?>
+    </a>
+    <?php if( $headline ) : ?>
+    <figcaption class="<?php echo $bg_color; ?>">
+        <b class="headline"><?php echo $headline; ?></b>
+        <?php if( $lead ) : ?><small class="lead"><?php echo $lead; ?></small><?php endif; ?>
+    </figcaption>
+    <?php endif; //headine ?>
+</figure>
+
+<?php endwhile; endif; ?>
+<?php endwhile; ?>
+<?php endif; //teasers loops ?>
+
+
 
 
 
