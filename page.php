be0763ee6dbcb8a4d3c5f916f1836e24d96b532f
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-29 18:56:46 +0700

    Page update (temp)
    Duplicated post script for now, will revert back. Just need to see if it works.

--- page.php
+++ page.php
@@ -1,5 +1,61 @@
 <?php get_header(); // includes nav and hero ?>
 
+<style>
+.article-body img  { 
+width:100%!important;
+object-fit:contain!important;
+height:auto;
+} 
+
+.article-body > p > a  { 
+color:#ee0099;
+} 
+</style>
+
+<?php if ( have_posts() ) :  while ( have_posts() ) : the_post();  ?>
+
+<?php get_template_part( 'snippets/snippet-hero' ); ?>
+<?php get_template_part( 'snippets/snippet-feature' ); ?>
+
+<?php if ( !empty( get_the_content() ) ) : ?>
+
+    <?php get_template_part( 'snippets/ad', 'header' ); ?>
+
+<section class="row-block bg-white grid grid-gap w-max">
+    <?php get_template_part( 'snippets/snippet', 'side-sharers' ); // col-1 ?>
+    <article class="article-body w-safe colspan-7">
+        <header class="header w-max">
+            <!-- <strong class="kicker">Kicker Category</strong> -->
+            <h1 class="title"><?php the_title(); ?></h1>
+            <?php if ( ! has_excerpt() ) { ?>
+            <?php } else {?>
+            <em class="description"><?php echo get_the_excerpt();?></em>
+            <?php } ?>
+        </header>
+        <?php get_template_part( 'snippets/snippet', 'byline' ); ?>
+
+        <aside id="chapters" class="bg-black">
+            <!-- Chapters appear Below -->
+        </aside>
+
+        <?php the_content(); ?>
+        <?php get_template_part( 'snippets/snippet', 'article-sharers' ); // col-4 ?>
+    </article>
+    <?php get_template_part( 'snippets/ad', 'sidebar' ); ?>
+</section>
+<?php endif; ?>
+<?php endwhile; endif; ?>
+
+<?php get_template_part( 'snippets/snippet', 'related' ); // col-4 ?>
+
+<?php get_footer();  ?>
+
+
+<?php /*
+
+
+<?php get_header(); // includes nav and hero ?>
+
 <?php if ( have_posts() ) :  while ( have_posts() ) : the_post();  ?>
 
 <article>
@@ -25,3 +81,5 @@
 <?php endwhile; endif; ?>
 
 <?php get_footer();  ?>
+
+*/?>
