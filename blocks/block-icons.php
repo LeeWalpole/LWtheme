13b922a42850d51ab1345290053c4bc80d0c6661
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2021-05-29 22:56:58 +0700

    remove echos and block-header

--- blocks/block-icons.php
+++ blocks/block-icons.php
@@ -5,7 +5,7 @@
 <div id="<?php // echo esc_attr($block_id); ?>" class="row-block prefade <?php // echo esc_attr($bg_color); ?>">
     <section class="<?php // echo esc_attr($width); ?> grid"><!-- .article-block -->
         <article class="article colspan-12">
-            <?php include ('block-header.php'); ?>
+            <?php // include ('block-header.php'); ?>
             <div class="grid icons <?php // echo $bg_color; ?>">
                 <?php include ('block-icon.php'); ?>
             </div>
