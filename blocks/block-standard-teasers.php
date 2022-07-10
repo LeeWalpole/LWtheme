294bebbb4e77b4d1fa0b6f807cff188d5ec9f76c
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-07-10 20:28:15 +0700

    teaser-standard

--- blocks/block-standard-teasers.php
+++ blocks/block-standard-teasers.php
@@ -1,5 +1,4 @@
-<h6>block-standard-teasers</h6>
-<?php /*
+
 
 <div class="standard-teasers teasers <?php echo $bg_color; ?>">
             <?php
@@ -15,7 +14,33 @@
     if ( $myposts ) {
         foreach ( $myposts as $post ) : 
             setup_postdata( $post ); ?>
-                <?php get_template_part( 'teaser-standard' ); ?>
+                <?php // get_template_part( 'teaser-standard' ); ?>
+
+    <?php 
+    $category = get_the_category(); $kicker_alt = $category[0]->name; // get category name
+    $kicker = $acf_kicker ?: get_the_excerpt(); // if acf_kicker exists, else get the category
+    $headline = $acf_headline ?: get_the_title(); // if acf_headline exists, else get the post title
+    $subdeck = $acf_headline ?: get_the_excerpt(); // if acf_headline exists, else get the post title
+    $teaser_image_url = get_the_post_thumbnail_url($post->ID, 'thumbnail') ? : "https://i0.wp.com/www.ladsholidayguide.com/wp-content/uploads/g-logo.png?w=360h=240&ssl=1";
+    ?>
+
+<a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr ( get_the_title ()); ?>" class="teaser bg-white">
+    <figure class="z-index-1">
+        <picture class="ratio" data-ratio="3:2">
+        <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+        alt="<?php echo esc_attr(get_the_title()); ?>"
+        data-src="<?php echo esc_attr($teaser_image_url); ?>"
+        loading="lazy" class="lazyload">
+        </picture>
+    </figure>
+    <header class="header">
+        <strong class="kicker"><?php echo $kicker; ?></strong>
+        <h4 class="headline"><?php echo $headline; ?></h4>
+        <p class="subdeck"><?php echo $subdeck; ?></p>
+    </header>
+    </a>   
+
+
                 <?php
         endforeach;
         wp_reset_postdata();
@@ -23,5 +48,4 @@
     ?>
             </div>
 
-    */?>
 
