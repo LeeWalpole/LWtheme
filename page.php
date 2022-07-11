a5eb29e9a8c316e576e202f11218ff5c8cfe6070
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-07-11 11:55:06 +0700

    Update page.php

--- page.php
+++ page.php
@@ -45,7 +45,7 @@ color:#ee0099;
 <?php endif; ?>
 <?php endwhile; endif; ?>
 
-<?php get_template_part( 'blocks/blocks' ); ?>
+<?php // get_template_part( 'blocks/blocks' ); ?>
 
 <?php // get_template_part( 'snippets/snippet', 'related' ); // col-4 ?>
 
