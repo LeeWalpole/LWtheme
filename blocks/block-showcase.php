e29d590524843c439c4d3f2c3891df51edd6bbb3
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-07-11 12:35:54 +0700

    Update block-showcase.php

--- blocks/block-showcase.php
+++ blocks/block-showcase.php
@@ -78,7 +78,7 @@ $showcase_heading = get_sub_field('showcase_heading');
     $posts = get_posts(array(
     'post_type'			=> 'post',
     'posts_per_page' => 0,
-    // 'offset' => 1,
+    'offset' => 1,
     'tag_id' => $teaser_tag, 
     'category' => $teaser_category ) ); 
     if( $posts ): ?>
@@ -94,7 +94,7 @@ $showcase_heading = get_sub_field('showcase_heading');
     $feature_youtube = get_field('feature_youtube'); 
     ?>
 
-            <?php $i++; if( $i >= 2 && $i <= 6  ) : ?>
+            <?php $i++; if( $i >= 1 && $i <= 6  ) : ?>
             <a href="<?php echo esc_url(get_permalink()); ?>" title="<?php echo esc_attr($headline); ?>"
                 class="puff_teaser teaser bg-white">
                 <figure>
