187c6f893620b937d49b510fb92278d1f3974fec
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2022-07-18 15:55:58 +0700

    Update single.php

--- single.php
+++ single.php
@@ -28,32 +28,44 @@
 
     <aside class="colspan-3 bg-white sidebar">
 
-    <ul class="sticky-sidebar sharers">
-        <li>
-            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url(get_permalink());?>" target="_blank" rel="noopener" class="prefade" title="Share <?php echo esc_attr(get_the_title()); ?> on Facebook.">
-                <i class="fab fa-facebook-f" aria-hidden="true"></i>
-            </a>
-        </li>
-        <li>
-            <a href="https://twitter.com/home?status=<?php echo esc_url(get_permalink());?>" target="_blank" rel="noopener" class="prefade" title="Tweet about the <?php echo esc_attr(get_the_title()); ?> article.">
-                <i class="fab fa-twitter" aria-hidden="true"></i>
-            </a>
-        </li>
-        <li>
-            <a href="whatsapp://send" data-text="Share" data-href="<?php echo esc_url(get_permalink());?>" rel="noopener" target="_blank" class="prefade" title="Send <?php echo esc_attr(get_the_title());?> to a mate.">
-                <i class="fab fa-whatsapp" aria-hidden="true"></i>
-            </a>
-        </li>
-        <li>
-            <a href="mailto:<?php echo esc_url(get_the_author_meta('user_email')); ?>?subject=<?php echo esc_url(get_the_title()); ?>&body=<?php echo esc_url(get_permalink());?>" class="prefade" title="Attach link to <?php echo esc_attr(get_the_title()); ?> in an email.">
-                <i class="fa fa-link"></i>
-            </a>
-        </li>
-    </ul>
+        <div class="sticky-sidebar ">
+            <ul class="sharers">
+                <li>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url(get_permalink());?>"
+                        target="_blank" rel="noopener" class="prefade"
+                        title="Share <?php echo esc_attr(get_the_title()); ?> on Facebook.">
+                        <i class="fab fa-facebook-f" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/home?status=<?php echo esc_url(get_permalink());?>" target="_blank"
+                        rel="noopener" class="prefade"
+                        title="Tweet about the <?php echo esc_attr(get_the_title()); ?> article.">
+                        <i class="fab fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="whatsapp://send" data-text="Share" data-href="<?php echo esc_url(get_permalink());?>"
+                        rel="noopener" target="_blank" class="prefade"
+                        title="Send <?php echo esc_attr(get_the_title());?> to a mate.">
+                        <i class="fab fa-whatsapp" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:<?php echo esc_url(get_the_author_meta('user_email')); ?>?subject=<?php echo esc_url(get_the_title()); ?>&body=<?php echo esc_url(get_permalink());?>"
+                        class="prefade" title="Attach link to <?php echo esc_attr(get_the_title()); ?> in an email.">
+                        <i class="fa fa-link"></i>
+                    </a>
+                </li>
 
-    <aside id="chapters" class="bg-black">
-            <!-- Chapters appear Below -->
-        </aside>
+            </ul>
+
+            <aside id="chapters" class="bg-black">
+                <!-- Chapters appear Below -->
+            </aside>
+
+
+        </div>
 
     </aside>
 
@@ -69,14 +81,12 @@
         </header>
         <?php get_template_part( 'snippets/snippet', 'byline' ); ?>
 
-
-
         <?php the_content(); ?>
         <?php get_template_part( 'snippets/snippet', 'article-sharers' ); // col-4 ?>
     </article>
 
     <aside class="colspan-3 bg-white sidebar">
-    <?php get_template_part( 'snippets/ad', 'sidebar' ); ?>
+        <?php get_template_part( 'snippets/ad', 'sidebar' ); ?>
     </aside>
 
 </section>
